8333aa1d59723f64a81292b0508ab3e5222f97be
BL-CoderOvertime <brandonlively@live.com>
2019-06-21 11:29:52 -0400

    Initial Commit

--- StartHere/src/main/java/com/lambdaschool/starthere/StartHereApplication.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/StartHereApplication.java
@@ -14,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class StartHereApplication
 {
+    //For Initial Commit
 
     public static void main(String[] args)
     {
